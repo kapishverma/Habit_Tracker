61583e50d2bee33f70a93f6f605c8bb05b4da891
Kapish Verma <122527683+kapishverma@users.noreply.github.com>
2023-08-10 11:59:26 +0530

    Update WeeklyHabitStatusComponent.jsx

--- src/Components/Weekly/WeeklyHabitStatusComponent.jsx
+++ src/Components/Weekly/WeeklyHabitStatusComponent.jsx
@@ -5,12 +5,14 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { db } from '../../Tools/firebase';
 import { useDispatch } from 'react-redux';
-import { addCurrentDay, deleteHabit, habitsAction } from '../../Redux/Reducers/HabitsReducer';
+import { addCurrentDay, deleteHabit, habitsAction, updateHabitStatus } from '../../Redux/Reducers/HabitsReducer';
+import { tostify } from '../../Tools/tostify';
 
 export default function WeeklyHabitStatusComponent(props) {
     const dispatch = useDispatch();
 
     const [currentWeek, setCurrentWeek] = useState(null);
+
     const { habit } = props;
     const { habitName, habitDocRefPath, daysCollectionRefPath } = habit;
 
@@ -18,17 +20,20 @@ export default function WeeklyHabitStatusComponent(props) {
         let unsubscribe;
         const currentDate = new Date();
 
+        // Set the current date to the beginning of Sunday
         const startOfWeek = new Date(currentDate);
         startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay());
+        startOfWeek.setHours(0, 0, 0, 0); // Set time to 12:00 AM
 
-        // Create Timestamps from startOfWeek and endOfWeek
-        const startTimestamp = Timestamp.fromMillis(startOfWeek.getTime());
+        // Create Timestamp from startOfWeek
+        const startTimestamp = Timestamp.fromDate(startOfWeek);
 
         const q = query(
             collection(db, daysCollectionRefPath),
             where('timeStamp', '>=', startTimestamp),
             orderBy('timeStamp')
         );
+
 
         async function callMe() {
             try {
@@ -61,9 +66,10 @@ export default function WeeklyHabitStatusComponent(props) {
                         } else {
                             await dispatch(addCurrentDay(daysCollectionRefPath));
                         }
-                    }else{
-                        return;
                     }
+                    // else {
+                    //     return;
+                    // }
                 });
             } catch (error) {
                 console.log(error)
@@ -79,27 +85,61 @@ export default function WeeklyHabitStatusComponent(props) {
 
 
     const setCurrentWeekHelperFunction = (fetchedData) => {
-
         const daysOfWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
-
         const modifiedData = [];
 
-        for (const dayOfWeek of daysOfWeek) {
+        let date = new Date();
+        date.setDate(date.getDate() - date.getDay()); // Move to the beginning of the week
 
+        for (const dayOfWeek of daysOfWeek) {
             const dayData = fetchedData.find(data => data.day === dayOfWeek);
 
             if (!dayData) {
-                modifiedData.push({ day: dayOfWeek, status: "none" });
+                const year = date.getFullYear();
+                const month = String(date.getMonth() + 1).padStart(2, '0');
+                const day = String(date.getDate()).padStart(2, '0');
+                const formattedDate = `${year}-${month}-${day}`; // e.g., "2023-08-10" format
+
+                // Convert JavaScript date to Firebase Timestamp
+                const firebaseTimestamp = Timestamp.fromDate(date);
+
+                modifiedData.push({
+                    date: formattedDate,
+                    day: dayOfWeek,
+                    status: "none",
+                    daysCollectionRefPath,
+                    timeStamp: firebaseTimestamp
+                });
+
             } else {
                 modifiedData.push(dayData);
             }
+            // Move to the next day
+            date.setDate(date.getDate() + 1);
         }
-        setCurrentWeek(modifiedData)
+        setCurrentWeek(modifiedData);
     }
 
-    const handleDelete = async () => {
-        await dispatch(deleteHabit(habitDocRefPath))
+
+
+    const handleStatusUpdate = async (dayData) => {
+        const daysOfWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+        let date = new Date();
+
+        if (date.getDay() < daysOfWeek.indexOf(dayData.day)) {
+            tostify("error", "Oops, hold your horses! Future updates pending")
+            return;
+        }
+
+        const newStatus = (dayData.status === "done" ? "not_done" : dayData.status === "not_done" ? "none" : "done");
+        await dispatch(updateHabitStatus({ ...dayData, newStatus }));
     }
+
+
+    const handleDelete = async () => {
+        await dispatch(deleteHabit(habitDocRefPath));
+    }
+
 
 
     return (<>
@@ -110,10 +150,11 @@ export default function WeeklyHabitStatusComponent(props) {
             </div>
             <div className={css.weeklyDetail}>
                 {currentWeek &&
-                    currentWeek.map((current, index) =>
+                    currentWeek.map((dayData, index) =>
                         <div key={index}
-                            style={{ backgroundColor: `${current.status === "done" ? "green" : current.status === "none" ? "grey" : "red"}` }}>
-                            {current.day}
+                            onClick={() => handleStatusUpdate(dayData)}
+                            style={{ backgroundColor: `${dayData.status === "done" ? "green" : dayData.status === "none" ? "grey" : "red"}` }}>
+                            {dayData.date.split("-")[2]}
                         </div>)
                 }
             </div>
